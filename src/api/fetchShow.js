45ad77df4deddd6f943c54e43cd2f08043451ac5
emma <sonicjane2020@gmail.com>
2020-03-23 17:21:53 -0700

    met mvp for real

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -2,10 +2,11 @@ import axios from 'axios';
 
 export const fetchShow = () => {
     return axios
-        .get('')
+        .get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
         .then((res) => {
-            return res;
-        }) .catch((err) => {
-            return err;
-        });
-};
+            return res
+        })
+        .catch((err) => {
+            return err
+        })
+}
